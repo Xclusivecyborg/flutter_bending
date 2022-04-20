bfa3cd98c42dc59729810cbce042bd4ad378ada6
Xclusivecyborg <xclusivecyborg@gmail.com>
2022-04-20 22:22:17 +0100

    dilo screen

--- lib/others/dilo_screen.dart
+++ lib/others/dilo_screen.dart
@@ -14,15 +14,51 @@ class DiloScreen extends StatelessWidget {
           CustomPaint(
             painter: DiloPainter(),
             child: Container(
-              padding: const EdgeInsets.only(top: 250, left: 30),
-              // color: Colors.blue,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.black,
-                  width: 2,
-                ),
-              ),
+              alignment: Alignment.bottomCenter,
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
               height: 400,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        height: 5,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      Container(
+                        height: 5,
+                        width: 20,
+                        decoration: BoxDecoration(
+                          color: Colors.black26,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      )
+                    ],
+                  ),
+                  Container(
+                    height: 35,
+                    width: 35,
+                    child: const Icon(
+                      Icons.arrow_forward,
+                      color: Colors.black,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 2,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
@@ -41,16 +77,15 @@ class DiloPainter extends CustomPainter {
     final paint1 = Paint()
       ..style = PaintingStyle.fill
       ..strokeWidth = 3
-      ..color = Colors.green;
+      ..color = Colors.grey[300]!;
 
     final firstPath = Path();
     firstPath.moveTo(0, size.height);
-    firstPath.lineTo(0, height * 0.3);
+    firstPath.lineTo(0, height * 0.34);
     firstPath.quadraticBezierTo(
-        width * 0.15, height * 0.1, width * 0.3, height * 0.2);
-    firstPath.quadraticBezierTo(
-        width * 0.15, height * 0.1, width * 0.3, height * 0.2);
-
+        width * 0.07, height * 0.09, width * 0.4, height * 0.16);
+    firstPath.quadraticBezierTo(width * 0.96, height * 0.24, width, 0);
+    firstPath.lineTo(width, height);
     canvas.drawPath(firstPath, paint1);
   }
 
